9584d41b422b694c2a677506126f6332a3eb946f
ozkansoyturk <soyturkozkan@gmail.com>
2023-12-04 15:09:33 +0100

    deploy

--- src/router.tsx
+++ src/router.tsx
@@ -6,16 +6,16 @@ import Gsap from "./pages/gsap/Gsap";
 
 export const router = createBrowserRouter([
   {
-    path: "/playground",
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/hyperplexed",
+        path: "hyperplexed", // Utilisez un chemin absolu ici
         element: <Hyperplexed />,
       },
       {
-        path: "/gsap",
+        path: "gsap", // Utilisez un chemin absolu ici
         element: <Gsap />,
       },
     ],
