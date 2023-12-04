2ef69bbf91814d45d5dbf95ff90256fe1f01db1f
ozkansoyturk <soyturkozkan@gmail.com>
2023-12-04 15:46:04 +0100

    deploy 7

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { Router } from "./router";
 import "@radix-ui/themes/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>
 );
--- src/router.tsx
+++ src/router.tsx
@@ -1,23 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./pages/errorPage/ErrorPage";
 import Hyperplexed from "./pages/hyperplexed/Hyperplexed";
 import Gsap from "./pages/gsap/Gsap";
 
-export const router = createBrowserRouter([
-  {
-    path: "/playground",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "hyperplexed", // Utilisez un chemin absolu ici
-        element: <Hyperplexed />,
-      },
-      {
-        path: "gsap", // Utilisez un chemin absolu ici
-        element: <Gsap />,
-      },
-    ],
-  },
-]);
+export const Router = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" errorElement={<ErrorPage />} element={<App />}>
+          <Route path="/hyperplexed" element={<Hyperplexed />} />
+          <Route path="/gsap" element={<Gsap />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
